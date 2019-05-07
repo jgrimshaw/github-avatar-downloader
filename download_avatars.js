e16a4c7e4d0773c6b58b0d6b4c795542617a9205
Jelena Grimshaw <muchstudio@hotmail.com>
2019-05-07 02:04:10 +0000

    prints each avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,9 @@
 var request = require('request');
-var token = require('./secrets.js')
-console.log('Welcome to the GitHub Avatar Downloader!');
+var token = require('./secrets.js');
+var fs = require("fs");
+
+// console.log('Welcome to the GitHub Avatar Downloader!')
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
@@ -10,25 +13,26 @@ function getRepoContributors(repoOwner, repoName, cb) {
       'Authorization': token.GITHUB_TOKEN
     }
   }
-
-  request(url, function(err, res, body) {
-    cb(err, body);
+  request(options, function(err, res, body) {
+    cb(err, body)
   });
-
 }
 
 
+getRepoContributors("jquery", "jquery", function(err, result) {
 
+  var parseResult = JSON.parse(result);
 
+  parseResult.forEach(function(elm){
+    console.log(elm.avatar_url)
+  })
+  // console.log("Errors:", err);
+  // console.log("Result:", parseResult);
 
-
-
-
-
-
-
+});
 
 // getRepoContributors("jquery", "jquery", function(err, result) {
 //   console.log("Errors:", err);
 //   console.log("Result:", result);
+
 // });
