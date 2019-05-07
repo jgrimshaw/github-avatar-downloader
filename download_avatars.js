8e230925474bf55d6a106cb1d3e91459f92262e1
Jelena Grimshaw <muchstudio@hotmail.com>
2019-05-07 03:08:26 +0000

    included command line arguments

--- download_avatars.js
+++ download_avatars.js
@@ -1,47 +1,48 @@
-var request = require('request');
-var token = require('./secrets.js');
-var fs = require("fs");
 
-// console.log('Welcome to the GitHub Avatar Downloader!')
 
+
+var owner = process.argv [2];
+var repo = process.argv [3];
+
+var request = require("request");
+var token = require("./secrets.js");
+var fs = require("fs")
+
+console.log("Welcome to the GitHub Avatar Downloader!");
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
-    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': 'request',
-      'Authorization': token.GITHUB_TOKEN
+      "User-Agent": "request",
+      "Authorization": token.GITHUB_TOKEN
     }
-  }
+  };
   request(options, function(err, res, body) {
-    cb(err, body)
+    cb(err, body);
   });
 }
 
 
 function downloadImageByURL(url, filePath){
-  request.get(url)
-    .on('error', function(err){
-        throw err;
-        })
-    .pipe(fs.createWriteStream(filePath));
+ request.get(url)
+  .on("error", function (err) {
+         throw err;
+  })
+  .pipe(fs.createWriteStream(filePath));
 }
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  var parseResult = JSON.parse(result);
-  parseResult.forEach(function(elm){
-    console.log(elm.login, elm.avatar_url)
+getRepoContributors(owner, repo, function(err, result) {
+ console.log("Errors:", err);
 
-    downloadImageByURL(elm.avatar_url, `avatars/${elm.login}.jpg`)
-  })
+ var parseResults = JSON.parse(result);
+
+parseResults.forEach(function(elm){
+  var login = elm.login
+  var avatar_url = elm.avatar_url
+
+  downloadImageByURL(avatar_url, `avatars/${login}.jpg`);
+})
 });
 
-
-
-
-// getRepoContributors("jquery", "jquery", function(err, result) {
-//   console.log("Errors:", err);
-//   console.log("Result:", result);
-
-// });
